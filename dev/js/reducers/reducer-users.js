0c52eb1826c084fc9eb2eb65ea7ae921f529d55a
Nemanja3089 <nemanjakuzmanovic1989@gmail.com>
2016-11-03 03:34:55 +0100

    Change users datas

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -7,26 +7,26 @@ export default function () {
     return [
         {
             id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
+            first: "Mike",
+            last: "Williams",
+            age: 35,
+            description: "He is a great and funny guy",
             thumbnail: "http://i.imgur.com/7yUvePI.jpg"
         },
         {
             id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
+            first: "John",
+            last: "Smith",
+            age: 29,
+            description: "John loves music and games.",
             thumbnail: "http://i.imgur.com/52xRlm8.png"
         },
         {
             id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
+            first: "Jena",
+            last: "Barkley",
+            age: 27,
+            description: "She is a party girl and also loves hooliday",
             thumbnail: "http://i.imgur.com/4EMtxHB.png"
         }
     ]
